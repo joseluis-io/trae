fd7b244e7922f94299d745ec1358ed475c914720
José Luis de la Hoz <git@joseluis.io>
2022-05-10 22:04:21 +0200

    Caching mechanism using a minimal key/value db
    User requested word definitions now are stored in a minimal key/value
    database using bolt. When requesting a word TRAE checks if it is stored
    to avoid requesting again the word, this also allows user to get word definitions
    already stored without internet connection.

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.17
 
 require github.com/EDDYCJY/fake-useragent v0.2.0
 
+require golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+
 require (
-	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/boltdb/bolt v1.3.1
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 )
--- main.go
+++ main.go
@@ -2,18 +2,47 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"net/http"
+	"os"
+
 	"github.com/PuerkitoBio/goquery"
+	"github.com/boltdb/bolt"
 )
 
 func main() {
 
+	// Open bolt database connection
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// create bucket if not exists
+	var bucket = []byte("trae")
+	db.Update(func(tx *bolt.Tx) error {
+		tx.CreateBucketIfNotExists(bucket)
+		return nil
+	})
+
+	// Get user input
 	word := os.Args[1]
+	// Prepare URL resource
 	baseURL := "https://dle.rae.es/" + word
 
-	// TODO: check if word exists in cache
+	// check if word exists in cache
+	exists := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(bucket)
+		v := b.Get([]byte(word))
+		fmt.Printf("%s\n", v)
+		if v != nil {
+			os.Exit(0)
+		}
+		return err
+	})
 
+	// Web Scraping
 	client := &http.Client{}
 	client.Transport = getTLSConfiguration(client.Transport)
 
@@ -27,21 +56,30 @@ func main() {
 	article := doc.Find("article").Text()
 	results := doc.Find("div.item-list").Text()
 
-	// TODO: persist word in cache
+	// Persist word in cache
+	if exists == nil {
+		db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket(bucket)
+			err := b.Put([]byte(word), []byte(article))
+			return err
+		})
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if article != "" {
 		fmt.Println(article)
-	}else{
+	} else {
 		fmt.Println(results)
 	}
 
-
 }
 
-func checkErr(err error){
-	if err != nil{
+func checkErr(err error) {
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
-
